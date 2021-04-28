a9e4bb1d7938409c004da529a95d8495e562614c
aplchian <alex.boquist@sovereign.co>
2021-04-28 14:31:38 -0400

    get price

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+export default async (req, res) => {
+    const OPTIONS = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+    const response = await fetch(`https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=ABML&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1`, OPTIONS)
+    if (response.ok) {
+        const data = await response.json()
+        return res.status(200).send(JSON.stringify({price: Number(data.FormattedQuoteResult.FormattedQuote[0].last)}))
+    } else {
+        throw new Error(response.statusText)
+    }
+
+
+  //   console.log('HI!!')
+  //   const xx = await fetch(`https://api.nasdaq.com/api/quote/ABML/info?assetclass=stocks`)
+  //
+  //   console.log('xx', xx)
+  //   const data = await xx.json()
+  //
+  //   console.lg('data', data)
+  //
+  //
+  //
+  // res.statusCode = 200
+  // res.json(data)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,30 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 import React from 'react'
-const client = new W3CWebSocket('wss://ws.finnhub.io?token=c24ospaad3ickpckcik0');
 
 export default function Home() {
     const [price, setPrice] = React.useState({color: 'black', price: null})
 
     React.useEffect(() => {
-        client.onopen = () => {
-            console.log('WebSocket Client Connected');
-            client.send(JSON.stringify({'type':'subscribe', 'symbol': 'TSLA'}))
-        };
-        client.onmessage = function(e) {
+
+        const fn = async () => {
+            const res = await fetch(`/api/hello/`)
+            const {price: currentPrice} = await res.json()
             try {
-            const currentPrice = JSON.parse(e.data).data[0].p
-            setPrice((current) => {
-                return {
-                    color: currentPrice > current.price ? 'green' : 'red',
-                    price: currentPrice
-                }
-            })
+
+                setPrice((current) => {
+                    return {
+                        color: currentPrice >= current.price ? 'green' : 'red',
+                        price: currentPrice
+                    }
+                })
             }catch(err) {
                 console.log(err)
             }
-        };
+        }
+
+        const int = setInterval(fn,2000)
 
     },[])
 
